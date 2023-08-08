5fd64370de92a21ba9d0b6a061cc7e590e4a6acf
Joan Amoros <joanamoros1999@gmail.com>
2023-08-08 10:00:47 +0200

    Day 1 Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::path::PathBuf;
 
-pub fn question1() -> u32 {
+fn calories() -> Vec<u32> {
     let mut path = PathBuf::from(std::env!("CARGO_MANIFEST_DIR"));
 
     path.push("inputs/adventofcode.com_2022_day_1_input.txt");
@@ -23,16 +23,35 @@ pub fn question1() -> u32 {
             .expect("Failed to parse line into u32");
     }
 
+    calories
+}
+
+pub fn question1() -> u32 {
+    let calories = calories();
     calories.into_iter().max().expect("Vec is empty, no max value")
+}
+
+pub fn question2() -> u32 {
+    let mut calories = calories();
+    calories.sort();
+
+    calories.into_iter().rev().take(3).sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::question1;
+    use super::{question1, question2};
 
     #[test]
-    fn test_answer_works_for_provided_input() {
+    fn test_answer_1_works_for_provided_input() {
         let answer = question1();
+        println!("Answer is {:?}", answer);
+        assert_ne!(answer, 0);
+    }
+
+    #[test]
+    fn test_answer_2_works_for_provided_input() {
+        let answer = question2();
         println!("Answer is {:?}", answer);
         assert_ne!(answer, 0);
     }
